20ee7715ed09b594d7a5793fbd0fc8350338873a
mburns98765 <mburns98765@gmail.com>
2024-09-26 21:32:01 +0100

    Python Menu Test to see if the colour sensor may be able to be used to detect the colour of the attachement and then use that to determine which program to run.
    This dosn't use PyBricks built-in menu function

---
+++ Coding/Python Menu Test.py
@@ -0,0 +1,44 @@
+import code1, code2, code3
+
+
+
+codesdict = {"A": ["Red", code1], "B": ["Blue", code2], "C": ["Green", code3]}
+
+
+def menu(codesdict):
+    selected = 0
+
+    coloursense = input("Colour Sense: ")
+    if coloursense in {key: value[0] for key, value in codesdict.items()}.values():
+        selected = list({key: value[0] for key, value in codesdict.items()}.values()).index(coloursense)
+    elif coloursense == "":
+        pass
+    else:
+        print("Invalid input")
+    codes = list(codesdict.keys())
+    print(codes[selected])
+    while True:
+        arrows = input("Navigation: ")
+        codecount = len(codes) - 1
+        if arrows == "<":
+            if selected <= 0:
+                selected = codecount
+            else:
+                selected -= 1
+            print(codes[selected])
+        elif arrows == ">":
+            if selected >= codecount:
+                selected = 0
+            else:
+                selected += 1
+            print(codes[selected])
+        elif arrows == "/":
+            return codes[selected]
+        else:
+            print("Invalid input")
+
+while True:
+    code = menu(codesdict)
+
+    if code in codesdict:
+        codesdict[code][1]()
