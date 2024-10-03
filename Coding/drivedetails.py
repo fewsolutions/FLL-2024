a168e3d486022d3e7c092cb3b9231cfa3431af2f
mburns2022 <155916311+mburns2022@users.noreply.github.com>
2024-10-03 16:25:58 +0100

    Create drivedetails.py
    Details for parameters of new robot

---
+++ Coding/drivedetails.py
@@ -0,0 +1,11 @@
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
+from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait, StopWatch
+
+hub = PrimeHub()
+
+driveL = Motor(port=Port.D, positive_direction=Direction.COUNTERCLOCKWISE)
+driveR = Motor(port=Port.B, positive_direction=Direction.CLOCKWISE)
+base = DriveBase(left_motor=driveL, right_motor=driveR, wheel_diameter=57.25, axle_track=99.09)
