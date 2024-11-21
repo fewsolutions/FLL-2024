4d6d0f010f3cc2d2ef4654a04bed132095b50cf1
mb2022 <mburns2022@smgsblarney.ie>
2024-11-21 16:29:30 +0000

    Start Octopus Run

--- Coding/A.py
+++ Coding/A.py
@@ -7,8 +7,6 @@ hub, b, auxL, auxR, colorsensor = setup()
 
 b.use_gyro(True)
 
-#b.straight(100)
-b.turn(-45)
-#b.settings(straight_speed=1000)
-b.straight(650)
-b.straight(-650)
+b.turn(-55)
+b.straight(-500)
+b.straight(350)
