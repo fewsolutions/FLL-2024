c52879b61b62afe53c9d493c57612dcb83784006
mb2022 <mburns2022@smgsblarney.ie>
2025-01-16 16:31:01 +0000

    Meeting 16th Jan 2025
    Update the Krill run with the remote

--- Coding/B.py
+++ Coding/B.py
@@ -2,21 +2,23 @@ from pybricks.parameters import Stop
 from pybricks.tools import wait
 
 #def code2():
+#Original Krill run
 from codes import setup
 hub, b, auxL, auxR, colorsensor = setup()
-
 b.use_gyro(True)
 
-b.turn(-42)
-b.straight(500)
-b.straight(-350)
-b.turn(23)
-b.straight(380)
-b.turn(32)
-b.straight(135)
-b.settings(turn_rate=50)
-b.turn(-85)
+b.turn(-45)
+b.straight(560)
+b.straight(-275)
+b.turn(37)
+b.straight(390)
+b.turn(34)
+b.straight(125)
+b.settings(turn_rate=100)
+b.turn(-100)
 b.settings(turn_rate=200)
-b.straight(400)
-b.turn(-18)
-b.straight(200)
+b.straight(350)
+b.turn(-23)
+b.straight(330)
+b.turn(22)
+
