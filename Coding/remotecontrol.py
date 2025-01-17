7aa3f7a3c864d3c58823b6942c66d7aa0b7534ef
mb2022 <mburns2022@smgsblarney.ie>
2025-01-17 12:22:59 +0000

    Update RC program
    Ror faster coding and support detecting motor rotation

--- Coding/remotecontrol.py
+++ Coding/remotecontrol.py
@@ -9,6 +9,8 @@ hub, b, auxL, auxR, colorsensor = setup()
 r = Remote()
 mode = 1
 r.light.on(Color.GREEN)
+auxL.reset_angle(angle=0)
+auxR.reset_angle(angle=0)
 
 while True:
     pressed = r.buttons.pressed()
@@ -23,51 +25,63 @@ while True:
             print("Mode 1")
             r.light.on(Color.GREEN)
 
-    if Button.LEFT in pressed:
-        print(f"{b.distance()} mm")
-
-    if Button.RIGHT in pressed:
-        print(f"{b.angle()} degrees")
-
     if Button.LEFT in pressed and Button.RIGHT in pressed:
-        print("Reset")
         b.reset()
+        auxL.reset_angle(angle=0)
+        auxR.reset_angle(angle=0)
+        wait(100)
 
     if mode == 1:
 
         if Button.LEFT_PLUS in pressed:
-            b.straight(10, then=Stop.NONE)
+            b.straight(5, then=Stop.NONE)
 
         elif Button.LEFT_MINUS in pressed:
-            b.straight(-10, then=Stop.NONE)
+            b.straight(-5, then=Stop.NONE)
 
         elif Button.RIGHT_PLUS in pressed:
-            b.turn(2, then=Stop.NONE)
+            b.turn(1, then=Stop.NONE)
         
         elif Button.RIGHT_MINUS in pressed:  
-            b.turn(-2, then=Stop.NONE)
+            b.turn(-1, then=Stop.NONE)
 
         else:
             b.stop()
 
+        if Button.LEFT in pressed and Button.RIGHT not in pressed:
+            print(f"b.straight({b.distance()})")
+            wait(100)
+
+        if Button.RIGHT in pressed and Button.LEFT not in pressed:
+            print(f"b.turn({b.angle()})")
+            wait(100)
+
     if mode == 2:
 
         if Button.LEFT_PLUS in pressed:
-            auxL.run(200)
+            auxL.run(100)
 
         elif Button.LEFT_MINUS in pressed:
-            auxL.run(-200)
+            auxL.run(-100)
 
         else:
             auxL.stop()
 
         if Button.RIGHT_PLUS in pressed:
-            auxR.run(200)
+            auxR.run(100)
         
         elif Button.RIGHT_MINUS in pressed:
-            auxR.run(-200)
+            auxR.run(-100)
 
         else:
             auxR.stop()
+
+        if Button.LEFT in pressed and Button.RIGHT not in pressed:
+            print(f"auxL.run_angle(400, {auxL.angle()})")
+            wait(100)
+
+        if Button.RIGHT in pressed and Button.LEFT not in pressed:
+            print(f"auxR.run_angle(400, {auxR.angle()})")
+            wait(100)
 
     wait(100)
