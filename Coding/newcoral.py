18547d10760e968af17fac72ea9ecf0943f8d725
mb2022 <mburns2022@smgsblarney.ie>
2025-01-21 16:31:35 +0000

    Meeting 21st Jan 2025

--- Coding/newcoral.py
+++ Coding/newcoral.py
@@ -1,15 +1,33 @@
 from pybricks.parameters import Stop
-from pybricks.tools import wait
+from pybricks.tools import wait, multitask, run_task
 
 
 # New Coral run
 from codes import setup
 hub, b, auxL, auxR, colorsensor = setup()
 
-auxR.run_angle(400, 200)
-b.turn(24)
-b.straight(735)
-b.turn(-117)
-auxR.run_angle(400, -90)
-b.straight(55)
+
+
+async def upL():
+    await auxL.run_angle(400, -300)
+
+async def upR():
+    await auxR.run_angle(400, 200)
+
+run_task(multitask(upL(), upR()))
+
+
+b.straight(250)
+b.turn(45)
+b.straight(425)
+b.turn(-45)
+b.straight(120)
+b.turn(-90)
+auxR.run_angle(400, -120)
+b.straight(130)
 b.straight(-10)
+auxR.run_angle(400, 150)
+b.straight(-125)
+b.turn(34)
+b.straight(205)
+b.straight(-253)
