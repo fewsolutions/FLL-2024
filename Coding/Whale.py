a7176948c1d63d8d03bb0355a3647e70612cd370
mb2022 <mburns2022@smgsblarney.ie>
2025-03-24 13:31:00 +0000

    Meeting 24th March

--- Coding/Whale.py
+++ Coding/Whale.py
@@ -9,18 +9,21 @@ hub, b, auxL, auxR, colorsensor = setup()
 b.use_gyro(True)
 
 def code5():
+    b.settings(turn_acceleration=750, turn_rate=750)
     b.turn(-30)
     b.straight(682)
     b.turn(72)
     wait(100)
-    b.settings(straight_speed=750, straight_acceleration=750)
-    b.straight(325)
+    b.settings(straight_speed=250, straight_acceleration=750)
+    b.straight(500)
     wait(500)
+    b.settings(straight_speed=750, straight_acceleration=750)
 
-    b.straight(-150)
-    b.turn(-70)
-    b.straight(-730)
+    	
     b.use_gyro(False)
+    b.straight(-200)
+    b.turn(-180)
+    b.straight(-730)
     b.turn(70)
 
-#code5()
+code5()
