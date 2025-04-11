53a605288f9b2dc90b7737255fd2aa9d8f334102
mburns98765 <mburns2022@smgsblarney.ie>
2025-04-11 22:44:11 +0100

    Lettering and small updates + QR Code

--- Coding/Shark.py
+++ Coding/Shark.py
@@ -15,13 +15,10 @@ def code3():
     b.straight(-112)
     b.turn(31.4)
     b.straight(225)
-
     b.straight(-143)
     b.turn(103)
     auxR.run_angle(400, -180)
     b.turn(41)
-
-
     b.settings(straight_speed=150)
     b.straight(147)
     b.settings(straight_speed=750)
@@ -29,14 +26,11 @@ def code3():
     auxR.run_angle(400, 57)
     b.straight(232)
     auxR.run_angle(400, 138)
-
     b.turn(56)
     b.straight(1040)
     b.turn(64)
     b.straight(531)
-
     auxL.run_target(400, 0)
     auxR.run_target(400, 0)
 
-    
 #code3()
